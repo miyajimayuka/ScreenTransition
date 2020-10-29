499498f018891fdcf353f031bd7d125c4865f8d3
s19B712 <s19b712@shohokuac.jp>
2020-10-29 17:09:18 +0900

    Initial commit

---
+++ app/src/main/java/jp/ac/shohoku/s19b712/MainView.java
@@ -0,0 +1,17 @@
+package jp.ac.shohoku.s19b712;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class MainView extends View {
+    public MainView(Context context) {
+        super(context);
+    }
+
+    public MainView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
